62c1c0a196046f679c9ce84b65a6f53e28c3d395
StZHY <40257501+StZHY@users.noreply.github.com>
2024-10-13 13:33:05 +0800

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -144,7 +144,7 @@ def feed_dict(cf_pairs, start, end):
 if __name__ == "__main__":
 
     """fix the random seed"""
-    seed = 2023
+    seed = 2024
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
